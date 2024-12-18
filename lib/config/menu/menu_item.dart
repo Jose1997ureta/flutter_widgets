5f8788e940e31d50d412ccc523766d566e0bd70e
Jose Ureta <jose.ureta@onroadts.com>
2024-12-18 16:36:31 -0500

    Se agrego la lista de opciones

---
+++ lib/config/menu/menu_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.icon});
+}
+
+const appMenuItem = <MenuItem>[
+  MenuItem(
+      icon: Icons.smart_button_outlined,
+      title: 'Botones',
+      subTitle: 'Varios botones en Flutter',
+      link: '/button'),
+  MenuItem(
+      icon: Icons.credit_card,
+      title: 'Tarjetas',
+      subTitle: 'Un contenedor estilizado',
+      link: '/card'),
+];

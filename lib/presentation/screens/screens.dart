14f0fe941ceac340766b3cb0401ee675733252f2
Jose Ureta <jose.ureta@onroadts.com>
2024-12-28 19:03:14 -0500

    Creacion de las plantillas page

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,11 @@
+export 'package:flutter_widgets/presentation/screens/app_tutorial/app_tutorial.screen.dart';
+
+export 'package:flutter_widgets/presentation/screens/animated/animated.screen.dart';
+export 'package:flutter_widgets/presentation/screens/infinite_scroll/infinite_scroll.screen.dart';
+export 'package:flutter_widgets/presentation/screens/progress/progress.screen.dart';
+export 'package:flutter_widgets/presentation/screens/snackbar/snackbar.screen.dart';
+export 'package:flutter_widgets/presentation/screens/ui_controls/ui_controls.screen.dart';
+
 export 'package:flutter_widgets/presentation/screens/buttons/button_screen.dart';
 export 'package:flutter_widgets/presentation/screens/cards/card_screen.dart';
 export 'package:flutter_widgets/presentation/screens/home/home_screen.dart';

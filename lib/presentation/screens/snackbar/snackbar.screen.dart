88a00a1e9061ef6c3c98dbb0c6144113db904b9d
Jose Ureta <jose.ureta@onroadts.com>
2024-12-29 11:56:22 -0500

    Se agrego el showAdaptiveDialog y showAboutDialog

--- lib/presentation/screens/snackbar/snackbar.screen.dart
+++ lib/presentation/screens/snackbar/snackbar.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackBarScreen extends StatelessWidget {
   const SnackBarScreen({super.key});
@@ -10,10 +11,30 @@ class SnackBarScreen extends StatelessWidget {
       content: const Text("Hola Mundo"),
       action: SnackBarAction(
         label: "Ok!!",
-        onPressed: () {},
+        onPressed: () {
+          context.go("/");
+        },
       ),
       duration: const Duration(seconds: 2),
     ));
+  }
+
+  void openDialog(BuildContext context) {
+    showAdaptiveDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => AlertDialog(
+              title: const Text("Estas seguro"),
+              content: const Text("Estas seguro de realizar esta accion"),
+              actions: [
+                TextButton(
+                    onPressed: () => context.pop(),
+                    child: const Text("Cancelar")),
+                FilledButton(
+                    onPressed: () => context.pop(),
+                    child: const Text("Aceptar")),
+              ],
+            ));
   }
 
   @override
@@ -21,6 +42,38 @@ class SnackBarScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("SnackBar y Dialog"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: FilledButton(
+                  onPressed: () {
+                    showAboutDialog(
+                        context: context,
+                        applicationName: "Show About Dialog",
+                        children: [
+                          const Text(
+                              "lorem ipsum dolor sit amet consectetur adipiscing elit"),
+                        ]);
+                  },
+                  child: const Text("Licencia usados"),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: FilledButton(
+                  onPressed: () => openDialog(context),
+                  child: const Text("Mostrar Dialogo"),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () => showSnackBar(context),
